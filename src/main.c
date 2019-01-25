38b6a6522a9dc46ce346d61468ab834fa9e4ed56
lupyuen <luppy@appkaki.com>
2019-01-25 13:03:32 +0800

    Runs OK, doesnt print Starting

--- src/main.c
+++ src/main.c
@@ -6,9 +6,14 @@ double r;
 
 int main(void)
 {	
+	enable_log();          //  Enable logging via Arm Semihosting. Note: ST Link must be connected or this line will hang.
+	debug_println("Starting..."); debug_force_flush();
+
 	r = x * y;             //  r = 123 * 456
 	debug_print_float(r);  //  Print the value of 123 * 456.
-	debug_println(""); debug_flush();
+
+	debug_println("");
+	debug_force_flush();   //  Flush the debug buffer before we halt.
 	for (;;) {}            //  Loop forever.
 }
 
